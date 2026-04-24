e22a4b70941710f3b7f013255e170c6e691b676c
sajanborle <sajan.borle@atishay.com>
2026-04-24 17:12:40 +0530

    update

--- app/database.py
+++ app/database.py
@@ -1,11 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://kanban_user:admin123@localhost:5432/kanban"
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True  # avoids stale connections
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
@@ -16,7 +26,6 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
--- app/utils/email.py
+++ app/utils/email.py
@@ -1,16 +1,28 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
 
-EMAIL = "sajan.borle@atishay.com"
-PASSWORD = "ndmn yvsk lgcb aixr"
+load_dotenv()
+
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASS = os.getenv("SMTP_PASS")
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+
 
 def send_email(to, subject, body):
     msg = MIMEText(body)
     msg["Subject"] = subject
-    msg["From"] = EMAIL
+    msg["From"] = FROM_EMAIL
     msg["To"] = to
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(EMAIL, PASSWORD)
-        server.sendmail(EMAIL, to, msg.as_string())
+    try:
+        with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SMTP_USER, SMTP_PASS)
+            server.sendmail(FROM_EMAIL, to, msg.as_string())
+
+    except Exception as e:
+        print("Email error:", e)
