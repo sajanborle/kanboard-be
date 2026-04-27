703d2ab2684a165963c9376d4a7f0e5bcd380740
sajanborle <sajan.borle@atishay.com>
2026-04-27 11:46:39 +0530

    cors error

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 from app.database import Base, engine
 from app.routes import auth, project, task, user, column
 
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(swagger_ui_init_oauth=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    )
+
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(project.router, prefix="/projects")
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app import models, schemas
 from app.database import SessionLocal
